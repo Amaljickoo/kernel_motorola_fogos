f0478c1cb2069b967da9bc32db70ecc9f026641f
prabhatkrmishra <mprabhat774@gmail.com>
2025-11-10 12:29:17 +0000

    Revert "[SQUASH] RCU: BACKPORTS and UPSTREAMS merge"
    This reverts commit 8054fe7019a283705407664e10c1725419e6a5f6.

--- include/trace/events/rcu.h
+++ include/trace/events/rcu.h
@@ -41,7 +41,7 @@ TRACE_EVENT(rcu_utilization,
 	TP_printk("%s", __entry->s)
 );
 
-#if defined(CONFIG_TREE_RCU)
+#if defined(CONFIG_TREE_RCU) || defined(CONFIG_PREEMPT_RCU)
 
 /*
  * Tracepoint for grace-period events.  Takes a string identifying the
@@ -425,35 +425,7 @@ TRACE_EVENT_RCU(rcu_fqs,
 		  __entry->cpu, __entry->qsevent)
 );
 
-/*
- * Tracepoint for RCU stall events. Takes a string identifying the RCU flavor
- * and a string identifying which function detected the RCU stall as follows:
- *
- *	"StallDetected": Scheduler-tick detects other CPU's stalls.
- *	"SelfDetected": Scheduler-tick detects a current CPU's stall.
- *	"ExpeditedStall": Expedited grace period detects stalls.
- */
-TRACE_EVENT(rcu_stall_warning,
-
-	TP_PROTO(const char *rcuname, const char *msg),
-
-	TP_ARGS(rcuname, msg),
-
-	TP_STRUCT__entry(
-		__field(const char *, rcuname)
-		__field(const char *, msg)
-	),
-
-	TP_fast_assign(
-		__entry->rcuname = rcuname;
-		__entry->msg = msg;
-	),
-
-	TP_printk("%s %s",
-		  __entry->rcuname, __entry->msg)
-);
-
-#endif /* #if defined(CONFIG_TREE_RCU) */
+#endif /* #if defined(CONFIG_TREE_RCU) || defined(CONFIG_PREEMPT_RCU) */
 
 /*
  * Tracepoint for dyntick-idle entry/exit events.  These take a string
@@ -502,14 +474,16 @@ TRACE_EVENT_RCU(rcu_dyntick,
  */
 TRACE_EVENT_RCU(rcu_callback,
 
-	TP_PROTO(const char *rcuname, struct rcu_head *rhp, long qlen),
+	TP_PROTO(const char *rcuname, struct rcu_head *rhp, long qlen_lazy,
+		 long qlen),
 
-	TP_ARGS(rcuname, rhp, qlen),
+	TP_ARGS(rcuname, rhp, qlen_lazy, qlen),
 
 	TP_STRUCT__entry(
 		__field(const char *, rcuname)
 		__field(void *, rhp)
 		__field(void *, func)
+		__field(long, qlen_lazy)
 		__field(long, qlen)
 	),
 
@@ -517,33 +491,35 @@ TRACE_EVENT_RCU(rcu_callback,
 		__entry->rcuname = rcuname;
 		__entry->rhp = rhp;
 		__entry->func = rhp->func;
+		__entry->qlen_lazy = qlen_lazy;
 		__entry->qlen = qlen;
 	),
 
-	TP_printk("%s rhp=%p func=%ps %ld",
+	TP_printk("%s rhp=%p func=%ps %ld/%ld",
 		  __entry->rcuname, __entry->rhp, __entry->func,
-		  __entry->qlen)
+		  __entry->qlen_lazy, __entry->qlen)
 );
 
 /*
  * Tracepoint for the registration of a single RCU callback of the special
- * kvfree() form.  The first argument is the RCU type, the second argument
+ * kfree() form.  The first argument is the RCU type, the second argument
  * is a pointer to the RCU callback, the third argument is the offset
  * of the callback within the enclosing RCU-protected data structure,
  * the fourth argument is the number of lazy callbacks queued, and the
  * fifth argument is the total number of callbacks queued.
  */
-TRACE_EVENT_RCU(rcu_kvfree_callback,
+TRACE_EVENT_RCU(rcu_kfree_callback,
 
 	TP_PROTO(const char *rcuname, struct rcu_head *rhp, unsigned long offset,
-		 long qlen),
+		 long qlen_lazy, long qlen),
 
-	TP_ARGS(rcuname, rhp, offset, qlen),
+	TP_ARGS(rcuname, rhp, offset, qlen_lazy, qlen),
 
 	TP_STRUCT__entry(
 		__field(const char *, rcuname)
 		__field(void *, rhp)
 		__field(unsigned long, offset)
+		__field(long, qlen_lazy)
 		__field(long, qlen)
 	),
 
@@ -551,12 +527,13 @@ TRACE_EVENT_RCU(rcu_kvfree_callback,
 		__entry->rcuname = rcuname;
 		__entry->rhp = rhp;
 		__entry->offset = offset;
+		__entry->qlen_lazy = qlen_lazy;
 		__entry->qlen = qlen;
 	),
 
-	TP_printk("%s rhp=%p func=%ld %ld",
+	TP_printk("%s rhp=%p func=%ld %ld/%ld",
 		  __entry->rcuname, __entry->rhp, __entry->offset,
-		  __entry->qlen)
+		  __entry->qlen_lazy, __entry->qlen)
 );
 
 /*
@@ -568,24 +545,27 @@ TRACE_EVENT_RCU(rcu_kvfree_callback,
  */
 TRACE_EVENT_RCU(rcu_batch_start,
 
-	TP_PROTO(const char *rcuname, long qlen, long blimit),
+	TP_PROTO(const char *rcuname, long qlen_lazy, long qlen, long blimit),
 
-	TP_ARGS(rcuname, qlen, blimit),
+	TP_ARGS(rcuname, qlen_lazy, qlen, blimit),
 
 	TP_STRUCT__entry(
 		__field(const char *, rcuname)
+		__field(long, qlen_lazy)
 		__field(long, qlen)
 		__field(long, blimit)
 	),
 
 	TP_fast_assign(
 		__entry->rcuname = rcuname;
+		__entry->qlen_lazy = qlen_lazy;
 		__entry->qlen = qlen;
 		__entry->blimit = blimit;
 	),
 
-	TP_printk("%s CBs=%ld bl=%ld",
-		  __entry->rcuname, __entry->qlen, __entry->blimit)
+	TP_printk("%s CBs=%ld/%ld bl=%ld",
+		  __entry->rcuname, __entry->qlen_lazy, __entry->qlen,
+		  __entry->blimit)
 );
 
 /*
@@ -617,12 +597,12 @@ TRACE_EVENT_RCU(rcu_invoke_callback,
 
 /*
  * Tracepoint for the invocation of a single RCU callback of the special
- * kvfree() form.  The first argument is the RCU flavor, the second
+ * kfree() form.  The first argument is the RCU flavor, the second
  * argument is a pointer to the RCU callback, and the third argument
  * is the offset of the callback within the enclosing RCU-protected
  * data structure.
  */
-TRACE_EVENT_RCU(rcu_invoke_kvfree_callback,
+TRACE_EVENT_RCU(rcu_invoke_kfree_callback,
 
 	TP_PROTO(const char *rcuname, struct rcu_head *rhp, unsigned long offset),
 
@@ -642,34 +622,6 @@ TRACE_EVENT_RCU(rcu_invoke_kvfree_callback,
 
 	TP_printk("%s rhp=%p func=%ld",
 		  __entry->rcuname, __entry->rhp, __entry->offset)
-);
-
-/*
- * Tracepoint for the invocation of a single RCU callback of the special
- * kfree_bulk() form. The first argument is the RCU flavor, the second
- * argument is a number of elements in array to free, the third is an
- * address of the array holding nr_records entries.
- */
-TRACE_EVENT_RCU(rcu_invoke_kfree_bulk_callback,
-
-	TP_PROTO(const char *rcuname, unsigned long nr_records, void **p),
-
-	TP_ARGS(rcuname, nr_records, p),
-
-	TP_STRUCT__entry(
-		__field(const char *, rcuname)
-		__field(unsigned long, nr_records)
-		__field(void **, p)
-	),
-
-	TP_fast_assign(
-		__entry->rcuname = rcuname;
-		__entry->nr_records = nr_records;
-		__entry->p = p;
-	),
-
-	TP_printk("%s bulk=0x%p nr_records=%lu",
-		__entry->rcuname, __entry->p, __entry->nr_records)
 );
 
 /*
@@ -763,7 +715,6 @@ TRACE_EVENT_RCU(rcu_torture_read,
  *	"Inc1": rcu_barrier() piggyback check counter incremented.
  *	"OfflineNoCB": rcu_barrier() found callback on never-online CPU
  *	"OnlineNoCB": rcu_barrier() found online no-CBs CPU.
- *	"OfflineNoCBQ": rcu_barrier() found offline no-CBs CPU with callbacks.
  *	"OnlineQ": rcu_barrier() found online CPU with callbacks.
  *	"OnlineNQ": rcu_barrier() found online CPU, no callbacks.
  *	"IRQ": An rcu_barrier_callback() callback posted on remote CPU.
