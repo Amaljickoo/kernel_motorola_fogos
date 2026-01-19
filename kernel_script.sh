65e338928110be4d4d50f898d5ab2b8487d2f505
prabhatkrmishra <mprabhat774@gmail.com>
2026-01-19 10:25:00 +0000

    kernel_script: net: retune for CUBIC + CAKE
    Shift network tuning from throughput-biased to latency-consistent
    behavior.
    
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- kernel_script.sh
+++ kernel_script.sh
@@ -25,11 +25,11 @@ echo fe > /sys/class/net/rmnet7/queues/rx-0/rps_cpus
 # Pin Wi-Fi RX to both big cores
 echo c0 > /sys/class/net/wlan0/queues/rx-0/rps_cpus
 
-# Network response is more important than network efficiency
-echo 128 > /proc/sys/net/core/netdev_budget
-echo 7000 > /proc/sys/net/core/netdev_budget_usecs
-echo 1 > /proc/sys/net/ipv4/tcp_low_latency
-echo 16384 > /proc/sys/net/ipv4/tcp_notsent_lowat
+# Push control packets earlier (safe, low overhead)
+echo 4096 > /proc/sys/net/ipv4/tcp_notsent_lowat
+echo 1 > /proc/sys/net/ipv4/tcp_ecn
+echo 0 > /proc/sys/net/ipv4/tcp_slow_start_after_idle
+echo 1 > /proc/sys/net/ipv4/tcp_no_metrics_save
 
 # Set governor settings for CPU scaling
 echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
