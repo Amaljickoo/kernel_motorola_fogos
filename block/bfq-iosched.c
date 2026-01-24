4d242c92cdcdc65eb68f164c6a92574e809db9b8
prabhatkrmishra <mprabhat774@gmail.com>
2026-01-24 10:25:00 +0000

    Revert "block, bfq: extend incomplete name of field on_st"
    This reverts commit b51c767192f1b126a7c4f392cec8a48be42736bf.
    
    Signed-off-by: prabhatkrmishra <mprabhat774@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -1062,7 +1062,7 @@ bfq_bfqq_resume_state(struct bfq_queue *bfqq, struct bfq_data *bfqd,
 
 static int bfqq_process_refs(struct bfq_queue *bfqq)
 {
-	return bfqq->ref - bfqq->allocated - bfqq->entity.on_st_or_in_serv -
+	return bfqq->ref - bfqq->allocated - bfqq->entity.on_st -
 		(bfqq->weight_counter != NULL);
 }
 
